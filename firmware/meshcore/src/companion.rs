a4fc0edccf23df5dc5284ee298bacd216d80838e
redengin <sholstei@yahoo.com>
2026-02-13 23:22:43 -0500

    in work

--- firmware/meshcore/src/companion.rs
+++ firmware/meshcore/src/companion.rs
@@ -75,5 +75,5 @@ struct MeshCoreService {
     #[characteristic(uuid = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E", read, notify)]
     pub tx: heapless::Vec<u8, BLE_MTU_MAX>,
     //------------------------------------------------------------------------------
-    // TODO extend to provide true BLE interface (will require a new user app)
+    // TODO extend to provide true BLE interface
 }
