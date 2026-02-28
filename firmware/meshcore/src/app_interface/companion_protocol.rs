1b591980f1619d81fbcf72ada9239d8e0a4e6412
redengin <sholstei@yahoo.com>
2026-02-28 02:50:43 -0500

    in work

--- firmware/meshcore/src/app_interface/companion_protocol.rs
+++ firmware/meshcore/src/app_interface/companion_protocol.rs
@@ -1,12 +1,13 @@
-use log::*;
-const TAG: &str = "Command Protocol";
+// use log::*;
+// const TAG: &str = "Command Protocol";
 
-const MAX_MESSAGE_LENGTH: usize = 200;
+/// TODO find design reference
+// const MAX_MESSAGE_LENGTH: usize = 200;
 
 /// Immplemented per zero-copy semantics (i.e. the object is mapped to the underlying buffer,
 /// rather than copied onto the stack)
-/// 
-/// Guided by 
+///
+/// Guided by
 /// https://github.com/meshcore-dev/MeshCore/blob/main/docs/companion_protocol.md#commands
 ///
 /// additional info from
@@ -19,16 +20,19 @@ pub enum CommandPacket<'buffer> {
         magic: &'buffer u8,
         /// b"mccli" padded with b'\0' per design
         // label: &'buffer [u8; 9],
-        label: &'buffer [u8],
+        label: &'buffer [u8; 9],
     },
 
     // SendChannelTxtMessage - TODO not described in public protocol
     SendChannelMessage {
+        /// b"0x00" per design
+        magic: &'buffer u8,
         channel_index: &'buffer u8,
         /// seconds since epoch (little-endian)
-        timestamp: &'buffer u32,
+        timestamp: Option<chrono::DateTime<chrono::Utc>>,
         /// utf-8 of variable length
-        message: &'buffer [u8; MAX_MESSAGE_LENGTH],
+        // message: &'buffer [u8; MAX_MESSAGE_LENGTH],
+        message: &'buffer [u8],
     },
 }
 
@@ -49,8 +53,10 @@ pub enum CommandPacketError {
 impl<'buffer> CommandPacket<'buffer> {
     pub fn from_bytes(
         bytes: &'buffer [u8],
-    ) -> Result<(CommandPacket<'buffer>, usize /* used bytes */), (CommandPacketError, usize /* used bytes */)>
-    {
+    ) -> Result<
+        (CommandPacket<'buffer>, usize /* used bytes */),
+        (CommandPacketError, usize /* used bytes */),
+    > {
         if bytes.len() < 1 {
             return Err((CommandPacketError::EmptyBuffer, 0));
         }
@@ -70,7 +76,7 @@ impl<'buffer> CommandPacket<'buffer> {
                         return Ok((
                             CommandPacket::AppStart {
                                 magic: &bytes[1],
-                                label: &bytes[2..11],
+                                label: bytes[2..11].try_into().unwrap(),
                             },
                             LEN,
                         ));
@@ -82,17 +88,30 @@ impl<'buffer> CommandPacket<'buffer> {
                         return Err((CommandPacketError::NotImplemented, 1));
                     }
 
-                    // &CommandPacketType::SendChannelMessage => {
-                    //     const MIN_LEN: usize = 8;
+                    &CommandPacketType::SendChannelMessage => {
+                        const MIN_LEN: usize = 8; // must be atleast one message byte
+                        if bytes.len() < MIN_LEN {
+                            return Err((CommandPacketError::IncompleteBuffer, 0));
+                        }
 
-                    //     return Ok((
-                    //         CommandPacket::AppStart {
-                    //             magic: bytes[1],
-                    //             label: bytes[2..11].try_into().unwrap(),
-                    //         },
-                    //         MIN_LEN,
-                    //     ));
-                    // }
+                        // for usability, convert the timestamp data to a DateTime
+                        let timestamp_epoch_s = i32::from_le_bytes(bytes[3..7].try_into().unwrap());
+                        // let timestamp = chrono::Utc.timestamp_opt(timestamp_epoch_s as i64, 0);
+                        let timestamp = chrono::DateTime::from_timestamp_secs(timestamp_epoch_s as i64);
+
+                        return Ok((
+                            CommandPacket::SendChannelMessage {
+                                magic: &bytes[1],
+                                channel_index: &bytes[2],
+                                timestamp: timestamp,
+                                // WTF - as there is no size parameter, assume we consume full buffer?
+                                message: &bytes[(MIN_LEN - 1)..],
+                            },
+                            // as there is no message size parameter, consume everything
+                            // NOTE - async producers will fail if they don't write full frames
+                            bytes.len()
+                        ));
+                    }
 
                     // if we haven't implemented the packet comprehender,
                     // treat it like an illegal header
@@ -109,43 +128,47 @@ mod command_packet_tests {
     use crate::app_interface::companion_protocol::CommandPacket;
 
     #[test]
-    fn test_from_bytes() {
-        // AppStart
-        {
-            const APP_START_DATA: [u8; 11] = [0x01, 0x03, 0x6d, 0x63, 0x63, 0x6c, 0x69, 0, 0, 0, 0];
-            match CommandPacket::from_bytes(&APP_START_DATA) {
-                Ok((packet, used)) => match packet {
-                    CommandPacket::AppStart { magic, label } => {
-                        assert_eq!(APP_START_DATA.len(), used);
-                        assert_eq!(0x03, *magic);
-                        assert_eq!(APP_START_DATA[2..11], *label);
-                    }
+    fn test_app_start_from_bytes() {
+        const TEST_VECTOR: [u8; 11] = [0x01, 0x03, 0x6d, 0x63, 0x63, 0x6c, 0x69, 0, 0, 0, 0];
+        match CommandPacket::from_bytes(&TEST_VECTOR) {
+            Ok((packet, used)) => match packet {
+                CommandPacket::AppStart { magic, label } => {
+                    assert_eq!(TEST_VECTOR.len(), used);
+                    assert_eq!(0x03, *magic);
+                    assert_eq!(TEST_VECTOR[2..11], *label);
+                }
 
-                    _ => panic!("failed to comprehend APP_START packet"),
-                },
-                Err(e) => panic!("unable to parse APP_START - {:?}", e),
-            }
+                _ => panic!("failed to comprehend APP_START packet"),
+            },
+            Err(e) => panic!("unable to parse APP_START - {:?}", e),
         }
-        //     // test that zerocopy does the right thing
-        //     let command = CommandPacketType::AppStart;
-        //     let bytes = command.as_bytes();
-        //     match CommandPacketType::try_ref_from_prefix(bytes) {
-        //         Ok(p) => assert_eq!(*p.0, CommandPacketType::AppStart),
-        //         Err(e) => panic!("zerocopy is broken, couldn't determine Ack packet type - {e}"),
-        //     }
-
-        //     // non-exhaustive - if zerocopy does it right once, it should
-        //     //  do the proper thing for all values
-
-        //     let illegal_bytes: [u8; 1] = [0xFF];
-        //     match CommandPacketType::try_ref_from_prefix(&illegal_bytes) {
-        //         Ok(p) => panic!("zerocopy is broken, illegal packet type returned {:?}", p),
-        //         Err(_) => (/* ignored */)
-        //     }
     }
 
     #[test]
-    fn test_into_bytes() {}
+    fn test_send_channel_message_from_bytes() {
+        const TEST_VECTOR: [u8; 12] = [
+            0x03, 0x00, 0x01, 0xD2, 0x02, 0x96, 0x49, 0x48, 0x65, 0x6C, 0x6C, 0x6F,
+        ];
+        match CommandPacket::from_bytes(&TEST_VECTOR) {
+            Ok((packet, used)) => match packet {
+                CommandPacket::SendChannelMessage {
+                    magic,
+                    channel_index,
+                    timestamp,
+                    message,
+                } => {
+                    assert_eq!(TEST_VECTOR.len(), used);
+                    assert_eq!(0x00, *magic);
+                    assert_eq!(1, *channel_index);
+                    assert_eq!(chrono::DateTime::from_timestamp_secs(1234567890).unwrap(), timestamp.expect("invalid time value"));
+                    assert_eq!(TEST_VECTOR[7..], *message);
+                }
+
+                _ => panic!("failed to comprehend APP_START packet"),
+            },
+            Err(e) => panic!("unable to parse APP_START - {:?}", e),
+        }
+    }
 }
 
 /// https://github.com/meshcore-dev/MeshCore/blob/main/docs/companion_protocol.md#commands
